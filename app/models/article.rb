67fb6a77343c622dc8c27e259c131172046e0039
Taguchi-ivi <k.taguchi@ivisystem.com>
2022-03-20 21:00:17 +0900

    add custom validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -9,12 +9,33 @@
 #  updated_at :datetime         not null
 #
 class Article < ApplicationRecord
-    validates :title,presence: true
-    validates :content,presence: true
+    # presence => 存在有無 ブランクだとエラー
+    # length => 長さ, minimumは最低でも2文字ないとエラー, maximuumは100以下でないとエラー
+    # uniqueness => 一意 同じものがないようにする場合はtrue, すでに存在する場合はエラー
+    # format => 特定の表現か確認,
+    validates :title, presence: true
+    validates :title, length: { minimum: 2, maximum: 100 }
+    validates :title, format: { with: /\A(?!\@)/ }
+    
+    validates :content, presence: true
+    validates :content, length: { minimum: 10}
+    validates :content, uniqueness: true
 
+    # 独自のバリデーション
+    validate :validate_title_content_length
+    
     def display_created_at
         # binding.pry
         I18n.l(self.created_at, format: :default)
     end
+
+    private
+
+        def validate_title_content_length
+            char_count = self.title.length + self.content.length
+            unless char_count > 100
+                errors.add(:content, '100文字以上で!')
+            end
+        end
 
 end
